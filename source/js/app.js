b3e3632b832bebf5a8726052a6f083dec510ef67
Alexander <dev@salex.pro>
2017-10-24 19:31:47 +0300

    start

---
+++ source/js/app.js
@@ -0,0 +1,9 @@
+/* 
+@codekit-prepend '../../bower_components/jquery/dist/jquery.min.js';
+@codekit-prepend '../../bower_components/foundation-sites/dist/js/plugins/foundation.core.min.js';
+@codekit-prepend '../../bower_components/foundation-sites/dist/js/plugins/foundation.util.mediaQuery.min.js';
+@codekit-prepend '../../bower_components/foundation-sites/dist/js/plugins/foundation.util.box.min.js';
+@codekit-prepend '../../bower_components/owl.carousel/dist/owl.carousel.min.js';
+*/
+
+// $(document).foundation();
